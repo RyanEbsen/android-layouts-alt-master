8936846c67e9338ad4a87ad1a22c711e9e142dd1
RyanEbsen <77689652+RyanEbsen@users.noreply.github.com>
2021-11-10 03:50:16 -0600

    final

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -3,6 +3,9 @@ package com.ualr.layoutassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void calculateTotal(View view){
+        int total = 0;
+
+        CheckBox checkBox0 = findViewById(R.id.checkBox0);
+        CheckBox checkBox1 = findViewById(R.id.checkBox1);
+        CheckBox checkBox2 = findViewById(R.id.checkBox2);
+        CheckBox checkBox3 = findViewById(R.id.checkBox3);
+
+        TextView textBox0 = findViewById(R.id.textBox0);
+        TextView textBox1 = findViewById(R.id.textBox1);
+        TextView textBox2 = findViewById(R.id.textBox2);
+        TextView textBox3 = findViewById(R.id.textBox3);
+
+        if(checkBox0.isChecked())
+        {
+            total += Integer.parseInt(textBox0.getText().toString());
+        }
+        if(checkBox1.isChecked())
+        {
+            total += Integer.parseInt(textBox1.getText().toString());
+        }
+        if(checkBox2.isChecked())
+        {
+            total += Integer.parseInt(textBox2.getText().toString());
+        }
+        if(checkBox3.isChecked())
+        {
+            total += Integer.parseInt(textBox3.getText().toString());
+        }
+
+        TextView textBox4 = findViewById(R.id.textView2);
+        textBox4.setText(String.valueOf(total));
+
     }
 }
